f985d2a479df3ab150efdc6912f6e609b7f39be6
Dustin Schau <dustinschau@gmail.com>
2018-11-10 11:46:51 -0600

    chore: try, try again

---
+++ scripts/publish-changes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FOLDER=$1
+WORKING_DIR="${CIRCLE_WORKING_DIRECTORY:-.}"
+
+cd $WORKING_DIR/starters/$FOLDER
+git init
+git remote add origin https://$GH_TOKEN@github.com/dschau/gatsby-starter-$FOLDER.git
+git fetch origin/master
+git add .
+git commit --message "chore: syncing with gatsbyjs/starters monorepo"
+git push origin master
