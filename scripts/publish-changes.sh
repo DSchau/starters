51d4d34a143036add385e49d3e29a0e5986a5458
Dustin Schau <dustinschau@gmail.com>
2018-11-12 09:37:55 -0600

    chore: use correct fetch

--- scripts/publish-changes.sh
+++ scripts/publish-changes.sh
@@ -5,7 +5,7 @@ WORKING_DIR="${CIRCLE_WORKING_DIRECTORY:-.}"
 cd $WORKING_DIR/starters/$FOLDER
 git init
 git remote add origin https://$GH_TOKEN@github.com/dschau/gatsby-starter-$FOLDER.git
-git fetch origin/master
+git fetch origin master
 git add .
 git commit --message "chore: syncing with gatsbyjs/starters monorepo"
 git push origin master
